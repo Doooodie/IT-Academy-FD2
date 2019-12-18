20b555f99572286153f9f8f6e3acfe4cdd5b2b1f
Kostya Yurkevich <unfair.slenderman@gmail.com>
2019-12-18 16:15:15 +0300

    Changed const names from camelcase to uppercase

--- hw1 (Payment)/main.js
+++ hw1 (Payment)/main.js
@@ -1,17 +1,17 @@
-const phonePrice = 69.99;
-const accessoryPrice = 5.99;
+const PHONE_PRICE = 69.99;
+const ACCESSORY_PRICE = 5.99;
 
 let balance = +prompt("Введите баланс Вашего счёта: ", 1000);
 
-let phoneSum = phonePrice + tax(phonePrice);
-let accessorySum = accessoryPrice + tax(accessoryPrice);
+let phoneSum = PHONE_PRICE + tax(PHONE_PRICE);
+let accessorySum = ACCESSORY_PRICE + tax(ACCESSORY_PRICE);
 
 let spendingThreshold = balance - getPhoneCost();
 
 let overallPrice = getAccessoryCost() + getPhoneCost();
 let restOfBalance = balance - overallPrice;
 
-while (balance < phonePrice * 2 || balance == null || balance == "") {
+while (balance < PHONE_PRICE * 2 || balance == null || balance == "") {
   balance = +prompt("Этих денег не хватит для покупки! Введите баланс снова: ", 1000);
 }
 
@@ -44,11 +44,11 @@ function getPhoneCost() {
 }
 
 alert(
-    "Стоимость одного телефона: " + showDollar() + phonePrice +
+    "Стоимость одного телефона: " + showDollar() + PHONE_PRICE +
     "\n" + "Вы смогли купить телефонов на: " + showDollar() + getPhoneCost() +
     "\n" + "Налоги на телефоны (включены в стоимость): " + showDollar() + tax(getPhoneCost()) +
     "\n" +
-    "\n" + "Стоимость одного аксессуара: " + showDollar() + accessoryPrice +
+    "\n" + "Стоимость одного аксессуара: " + showDollar() + ACCESSORY_PRICE +
     "\n" + "Вы смогли купить аксессуаров на: " + showDollar() + getAccessoryCost() +
     "\n" + "Налоги на аксессуары (включены в стоимость): " + showDollar() + tax(getAccessoryCost()) +
     "\n" +
