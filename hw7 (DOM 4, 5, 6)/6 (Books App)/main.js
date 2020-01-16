373d6db95cd9ac1e21cdf20484ff4b231816c757
Kostya Yurkevich <unfair.slenderman@gmail.com>
2020-01-16 17:14:46 +0300

    Added half-working live search

--- hw7 (DOM 4, 5, 6)/6 (Books App)/main.js
+++ hw7 (DOM 4, 5, 6)/6 (Books App)/main.js
@@ -1,19 +1,50 @@
 let container = document.querySelector(".container");
-let ul = document.querySelector("ul");
+let searchBooks = document.querySelector("#SearchBooks");
+let ulBooks = document.querySelector("ul");
 let liBooks = document.querySelectorAll("li");
 let delBooks = document.querySelectorAll("li > button");
 let hideBooks = document.querySelector("#hide");
-
-for (let i = 0; i < delBooks.length; i++) {
-  delBooks[i].onclick = function() {
-    liBooks[i].remove();
-  };
-}
+let addBook = document.querySelector("#AddBook");
+let addBookByName = document.querySelector("#AddBookByName");
 
 hideBooks.onclick = function() {
   if (hideBooks.checked == true) {
-    ul.remove();
+    ulBooks.remove();
   } else {
-    container.append(ul);
+    container.append(ulBooks);
+  }
+};
+
+addBook.onclick = function() {
+  let newBook = liBooks[0].cloneNode(true);
+  let newBookName = newBook.firstElementChild;
+  newBookName.innerText = addBookByName.value;
+  ulBooks.append(newBook);
+};
+
+searchBooks.onfocus = function() {};
+
+searchBooks.oninput = function() {
+  let val = this.value.trim();
+  if (val != "") {
+    liBooks.forEach(function(elem) {
+      if (elem.innerText.search(val) == -1) {
+        elem.classList.add("hide");
+        elem.innerHTML = elem.innerText;
+      } else {
+        elem.classList.remove("hide");
+        let str = elem.innerText;
+        elem.innerHTML = insertMark(
+          str,
+          elem.innerText.search(val),
+          val.length
+        );
+      }
+    });
+  } else {
+    liBooks.forEach(function(elem) {
+      elem.classList.remove("hide");
+      elem.innerHTML = elem.innerText;
+    });
   }
 };
