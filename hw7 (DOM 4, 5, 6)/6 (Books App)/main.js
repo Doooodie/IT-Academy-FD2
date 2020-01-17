f2773f55e75bc2ca5cbf29cdaa37333a7b521ede
Kostya Yurkevich <unfair.slenderman@gmail.com>
2020-01-17 17:06:33 +0300

    Working live search

--- hw7 (DOM 4, 5, 6)/6 (Books App)/main.js
+++ hw7 (DOM 4, 5, 6)/6 (Books App)/main.js
@@ -1,11 +1,17 @@
 let container = document.querySelector(".container");
-let searchBooks = document.querySelector("#SearchBooks");
+let searchBooks = document.querySelector("header > input");
 let ulBooks = document.querySelector("ul");
 let liBooks = document.querySelectorAll("li");
-let delBooks = document.querySelectorAll("li > button");
 let hideBooks = document.querySelector("#hide");
-let addBook = document.querySelector("#AddBook");
-let addBookByName = document.querySelector("#AddBookByName");
+let addBookButton = document.querySelector("#add-book-button");
+let addBookInput = document.querySelector("#add-book-input");
+
+ulBooks.addEventListener("click", function(event) {
+  if (event.target.innerText == "Delete") {
+    let bookItem = event.target.parentElement;
+    bookItem.remove();
+  }
+});
 
 hideBooks.onclick = function() {
   if (hideBooks.checked == true) {
@@ -15,36 +21,29 @@ hideBooks.onclick = function() {
   }
 };
 
-addBook.onclick = function() {
+addBookButton.onclick = function() {
   let newBook = liBooks[0].cloneNode(true);
   let newBookName = newBook.firstElementChild;
-  newBookName.innerText = addBookByName.value;
+  newBookName.innerText = addBookInput.value;
   ulBooks.append(newBook);
 };
 
-searchBooks.onfocus = function() {};
-
 searchBooks.oninput = function() {
-  let val = this.value.trim();
-  if (val != "") {
+  if (this.value != "") {
     liBooks.forEach(function(elem) {
-      if (elem.innerText.search(val) == -1) {
+      if (
+        elem.firstElementChild.innerText
+          .toLowerCase()
+          .indexOf(searchBooks.value) == -1
+      ) {
         elem.classList.add("hide");
-        elem.innerHTML = elem.innerText;
       } else {
         elem.classList.remove("hide");
-        let str = elem.innerText;
-        elem.innerHTML = insertMark(
-          str,
-          elem.innerText.search(val),
-          val.length
-        );
       }
     });
   } else {
     liBooks.forEach(function(elem) {
       elem.classList.remove("hide");
-      elem.innerHTML = elem.innerText;
     });
   }
 };
