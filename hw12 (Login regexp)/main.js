839f7d6d2a2284ad697eaf0c8942931191ca2872
Kostya Yurkevich <unfair.slenderman@gmail.com>
2020-02-06 16:51:06 +0300

    Working validation

--- hw12 (Login regexp)/main.js
+++ hw12 (Login regexp)/main.js
@@ -3,7 +3,10 @@ var password = document.querySelectorAll("input")[1];
 var button = document.querySelectorAll("input")[2];
 
 function loginCheck() {
-
+    var r = login.value.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
+    if (!r){
+        return false
+    }
 }
 
 function passwordCheck() {
@@ -19,5 +22,10 @@ button.onclick = function () {
         password.style.borderColor = "red";
     } else {
         password.style.borderColor = "";
+    }
+    if (loginCheck() === false) {
+        login.style.borderColor = "red";
+    } else {
+        login.style.borderColor = "";
     }
 };
